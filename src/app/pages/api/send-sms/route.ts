cc48806471fee2a6cbbf9e000e99d7a773533e5f
zanzen <olsencortuna@gmail.com>
2024-11-14 15:29:09 +0800

    to fix api

--- src/app/pages/api/send-sms/route.ts
+++ src/app/pages/api/send-sms/route.ts
@@ -3,7 +3,8 @@ import axios from "axios";
 
 // Replace with your Semaphore API key
 const apiKey = process.env.SEMAPHORE_API_KEY;
-const senderName = process.env.SEMAPHORE_SENDER_NAME || 'SENDER_NAME';
+// const senderName = process.env.SEMAPHORE_SENDER_NAME || 'SEMAPHORE';
+const senderName = "Semaphore";
 
 export async function POST(req: Request) {
   if (!apiKey) {
@@ -21,15 +22,18 @@ export async function POST(req: Request) {
     } = await req.json();
 
     // Send the SMS via Semaphore
-    const response = await axios.post('https://api.semaphore.co/api/v4/messages', {
-      apikey: apiKey,
-      number: `+63${contact}`, // Sending to a dynamic contact number provided
-      message: messageBody,
-      sendername: senderName // Optional; only if sender name is configured in Semaphore
-    });
+    const response = await axios.post(
+      "https://api.semaphore.co/api/v4/messages",
+      {
+        apikey: apiKey,
+        number: `0${contact}`, // Sending to a dynamic contact number provided
+        message: messageBody,
+        sendername: senderName, // Optional; only if sender name is configured in Semaphore
+      }
+    );
 
     // Log the response for debugging purposes
-    console.log('Semaphore response', response.data);
+    console.log("Semaphore response", response.data);
 
     // Respond with success if the message was sent
     return NextResponse.json({ success: true, message: response.data });
